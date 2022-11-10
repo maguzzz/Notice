7c491cc657a8b9904f03c291439820487d63dda7
maguzzz <markusm.touray@gmail.com>
2022-11-10 11:24:22 +0100

    text change

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Notice</title>
+    <title>Notes</title>
     <?php require_once "components/boot.php" ?>
 </head>
 
